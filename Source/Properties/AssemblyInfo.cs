317abc0301782f99c5250da085e9303464e8993c
Timtech <timothy@timtechsoftware.com>
2015-05-29 17:45:27 -0400

    Added source code

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Packet Scout")]
+[assembly: AssemblyDescription("Listens on and shows traffic for a specified port")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TimTech Software")]
+[assembly: AssemblyProduct("Packet Scout")]
+[assembly: AssemblyCopyright("Copyright © 2015 TimTech Software")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e947d3a-33d9-4329-bc39-3b006b9df7a4")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
